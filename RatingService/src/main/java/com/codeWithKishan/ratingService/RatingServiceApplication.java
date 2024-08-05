bd594da7cbb06c5f4b1ae2722011b00f079294c2
kishank <kishank@kishank.vakrangee.local>
2024-08-05 11:52:45 +0530

    ok

--- RatingService/src/main/java/com/codeWithKishan/ratingService/RatingServiceApplication.java
+++ RatingService/src/main/java/com/codeWithKishan/ratingService/RatingServiceApplication.java
@@ -8,9 +8,6 @@ public class RatingServiceApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Rating service fire mode on ");
-		System.out.println(" two ");
-		System.out.println("three ");
-		System.out.println("four");
 		SpringApplication.run(RatingServiceApplication.class, args);
 	}
 
